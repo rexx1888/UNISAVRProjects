e1318a79b59245f99f11af2260c758db971362f4
Cam Schriever <cammo8778@gmail.com>
2019-04-29 16:12:39 +0930

    Beginning heatmap shader. Array is a bit limiting tho...

--- Assets/RenderViewData.cs
+++ Assets/RenderViewData.cs
@@ -11,7 +11,10 @@ public class RenderViewData : MonoBehaviour {
 	[SerializeField] private Analytics analytics;
 
 	//store the different rooms
-	[SerializeField] private List<Transform> raycastTrackerObjects;
+	[SerializeField] private List<GameObject> raycastTrackerObjects;
+
+
+
 	private List<List<Analytic>> raycastAnalyticSorted;
 	//The Rooms and the list of lists are stored the same, so say theres 3 rooms,
 	//raycastAnalyticSorted[x] will store the list of analytics referencing raycastTrackerObjects[x]
@@ -57,49 +60,65 @@ public class RenderViewData : MonoBehaviour {
 				}
 			}
 
-
-			//iterate through the sorted array per room
-			for (int x = 0; x < raycastAnalyticSorted.Count; x++)
-			{
-				//for now we are using a line renderer to show how the user looks around the room.
-				if (raycastAnalyticSorted[x].Count > 0)
-				{
-					GameObject connectionGO;
-					connectionGO = new GameObject(raycastAnalyticSorted[x][0].Room + " Connections");
-
-					//setup line renderer
-					LineRenderer lr = connectionGO.AddComponent<LineRenderer>();
-					lr.startWidth = 0.2f;
-					lr.endWidth = 0.2f;
-					lr.useWorldSpace = true;
-					lr.positionCount = raycastAnalyticSorted[x].Count;
-					lr.material = matTest;
-					lr.startColor = Color.green;
-					lr.endColor = Color.red;
-
-
-					//Iterate through the analytics list.
-					//if the data point's name is this room
-					for (int i = 0; i < raycastAnalyticSorted[x].Count; i++)
-					{
-						//Debug.Log("New point in " + raycastAnalyticSorted[x][i].Room);
-						//add a point on the line renderer
-						lr.SetPosition(i, raycastAnalyticSorted[x][i].Point);
-
-						//Instantiate the point.
-						GameObject pointText = Instantiate(pointPrefab);
-						TimePointScript tps = pointText.GetComponent<TimePointScript>();
-						tps.OnCreatePoint(raycastAnalyticSorted[x][i].Point, raycastTrackerObjects[x], raycastAnalyticSorted[x][i].TimeStamp);
-					}
-
-					//set the parent for the line renderer
-					connectionGO.transform.parent = raycastTrackerObjects[x];
-				}
-			}			
+			//CreateLineRender);
+			CreateHeatMap();
+			
 		}
 		else
 		{
 			Debug.Log("Analytics don't exist");
+		}
+	}
+
+	private void CreateLineRender()
+	{
+		//iterate through the sorted array per room
+		for (int x = 0; x < raycastAnalyticSorted.Count; x++)
+		{
+			//for now we are using a line renderer to show how the user looks around the room.
+			if (raycastAnalyticSorted[x].Count > 0)
+			{
+				GameObject connectionGO;
+				connectionGO = new GameObject(raycastAnalyticSorted[x][0].Room + " Connections");
+
+				//setup line renderer
+				LineRenderer lr = connectionGO.AddComponent<LineRenderer>();
+				lr.startWidth = 0.2f;
+				lr.endWidth = 0.2f;
+				lr.useWorldSpace = true;
+				lr.positionCount = raycastAnalyticSorted[x].Count;
+				lr.material = matTest;
+				lr.startColor = Color.green;
+				lr.endColor = Color.red;
+
+
+				//Iterate through the analytics list.
+				//if the data point's name is this room
+				for (int i = 0; i < raycastAnalyticSorted[x].Count; i++)
+				{
+					//Debug.Log("New point in " + raycastAnalyticSorted[x][i].Room);
+					//add a point on the line renderer
+					lr.SetPosition(i, raycastAnalyticSorted[x][i].Point);
+
+					//Instantiate the point.
+					GameObject pointText = Instantiate(pointPrefab);
+					TimePointScript tps = pointText.GetComponent<TimePointScript>();
+					tps.OnCreatePoint(raycastAnalyticSorted[x][i].Point, raycastTrackerObjects[x].transform, raycastAnalyticSorted[x][i].TimeStamp);
+				}
+
+				//set the parent for the line renderer
+				connectionGO.transform.parent = raycastTrackerObjects[x].transform;
+			}
+		}
+	}
+
+	private void CreateHeatMap()
+	{
+		//foreach (GameObject room in raycastTrackerObjects)
+		for (int i = 0; i < raycastTrackerObjects.Count; i++)
+		{
+			HeatMap heatMap = raycastTrackerObjects[i].GetComponent<HeatMap>();
+			heatMap.setData(raycastAnalyticSorted[i]);
 		}
 	}
 }
---
+++ Assets/heatmaps/Heatmap.shader
@@ -0,0 +1,86 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Alan Zucconi
+// www.alanzucconi.com
+
+
+Shader "Hidden/Heatmap" {
+	Properties{
+		_HeatTex("Texture", 2D) = "white" {}
+	}
+	
+	SubShader {
+		Tags { "Queue" = "Transparent" }
+		Blend SrcAlpha OneMinusSrcAlpha // Alpha blend
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert             
+			#pragma fragment frag
+			#define arrayLength 2000
+
+			struct vertInput {
+				float4 pos : POSITION;
+			};
+
+			struct vertOutput {
+				float4 pos : POSITION;
+				fixed3 worldPos : TEXCOORD1;
+			};
+
+			vertOutput vert(vertInput input) {
+				vertOutput o;
+				o.pos = UnityObjectToClipPos(input.pos);
+				o.worldPos = mul(unity_ObjectToWorld, input.pos).xyz;
+				return o;
+			}
+
+			uniform int _Points_Length = 0;
+			uniform float4 _Points[arrayLength];		// (x, y, z) = position
+			uniform float2 _Properties[arrayLength];	// x = radius, y = intensity
+
+			uniform float4 _Points2[arrayLength];
+			uniform float2 _Properties2[arrayLength];
+
+			sampler2D _HeatTex;
+
+			half4 frag(vertOutput output) : COLOR {
+				// Loops over all the points
+				half h = 0;
+				for (int i = 0; i < _Points_Length; i++)
+				{
+					if (i < arrayLength)
+					{
+						// Calculates the contribution of each point
+						half di = distance(output.worldPos, _Points[i].xyz);
+
+						half ri = _Properties[i].x;
+						half hi = 1 - saturate(di / ri); //Blend tolerance between points.
+
+						h += hi * _Properties[i].y;
+					}
+					//else if (i > arrayLength && i < arrayLength * 2)
+					//{
+					//	// Calculates the contribution of each point
+					//	half di = distance(output.worldPos, _Points2[i].xyz);
+
+					//	half ri = _Properties2[i].x;
+					//	half hi = 1 - saturate(di / ri); //Blend tolerance between points.
+
+					//	h += hi * _Properties2[i].y;
+					//}
+					
+				}
+
+				// Converts (0-1) according to the heat texture
+				h = saturate(h);
+				//then uses h to pick a point along the line, according to intensity.
+				half4 color = tex2D(_HeatTex, fixed2(h, 0.5));
+				return color;
+			}
+			ENDCG
+		}
+	}
+	Fallback "Diffuse"
+}
