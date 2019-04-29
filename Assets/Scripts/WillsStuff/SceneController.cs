d974f19722cd1e33efc7cbce0fbf07576895a786
UNISA/Designer-David <DavidJamesMcCann@gmail.com>
2019-04-29 17:47:01 +0930

    completed refactor of in Scene UI

--- Assets/Scripts/WillsStuff/SceneController.cs
+++ Assets/Scripts/WillsStuff/SceneController.cs
@@ -15,7 +15,6 @@ using VRStandardAssets.Utils;
 
 public class SceneController : MonoBehaviour {
 
-    public List<DisplayObject> baseObjectList; //the list of skybox dependent objects
     public DisplayObject userInterface;
     public GameObject player;
     public CurrentSceneState currentSceneState;
@@ -25,26 +24,31 @@ public class SceneController : MonoBehaviour {
     public float userInterfaceDistance;
     [SerializeField]protected VRInput c_Input;
 
+    protected List<ChangeSkyBox> buttons = new List<ChangeSkyBox>();
+
     protected SceneStateChangeStuff sceneStateCheck;
 
     public void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
         c_Input.OnHold += ShowUI;
-             
+        sceneStateCheck = currentSceneState.currentScene;     
         foreach(SceneStateChangeStuff sSCS in currentSceneState.sceneStuff)
         {
             sSCS.spawnedAnalyticTracker = Instantiate(sSCS.analyticTrackerObject, player.transform.position, player.transform.rotation).GetComponent<DisplayObject>();
             GetComponent<RenderViewData>().raycastTrackerObjects.Add(sSCS.spawnedAnalyticTracker.gameObject.transform);
         }
-        SwitchRoom();
-        foreach(ChangeSkyBox cSB in gameObject.GetComponentsInChildren<ChangeSkyBox>())
+        userInterface.SetActive(true);
+        foreach (ChangeSkyBox cSB in gameObject.GetComponentsInChildren<ChangeSkyBox>())
         {
+            buttons.Add(cSB);
             cSB.vrII = GetComponent<VRInteractiveItem>();
             cSB.controller = gameObject.GetComponent<SceneController>();
+            cSB.gameObject.AddComponent<DisplayObject>();
 
         }
-        userInterface.SetActive(true);
+        SwitchRoom();
+
         GetComponentInChildren<FinishApplication>().rvd = GetComponent<RenderViewData>();
         GetComponentInChildren<CloseUI>().userInterface = userInterface;
         userInterface.SetActive(false);
@@ -55,32 +59,11 @@ public class SceneController : MonoBehaviour {
 
         if(currentSceneState.currentScene != sceneStateCheck) //if the scene is supposed to change
         {
+            Debug.Log("state change");
             SwitchRoom();
         }
 
     }
-
-    //enables the object passed in if its in the list, and disables all others in the list that are not that one
-    public void EnableObject(DisplayObject objToEnable)
-    {
-
-        //foreach gameobject in the list
-        foreach (DisplayObject go in baseObjectList)
-        {
-            //if the passed in one is the same as the current iteration
-            if (go == objToEnable)
-            {
-                //enabled the object
-                go.SetActive(true);
-            } else
-            {
-                //disable the object if it is not the same as the one passed in
-                go.SetActive(false);
-            }
-        }
-    }
-
-    
 
     //displays the UI in front the user when they 'hold' down the control(to set Hold time, check VRInput)
     public void ShowUI()
@@ -102,6 +85,18 @@ public class SceneController : MonoBehaviour {
             if (currentSceneState.currentScene != sSCS)
             {
                 sSCS.spawnedAnalyticTracker.SetActive(false);
+            }
+        }
+
+        foreach(ChangeSkyBox cSB in buttons)
+        {
+            if(currentSceneState.currentScene != cSB.sceneSpecificStuff)
+            {
+                cSB.gameObject.GetComponent<DisplayObject>().SetActive(true);
+            }else
+            {
+                cSB.gameObject.GetComponent<DisplayObject>().SetActive(false);
+
             }
         }
     }
