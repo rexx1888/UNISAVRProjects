9e7cc4ec4c942a0ef8534584d8e97a9fbbbc7349
UNISA/Designer-David <DavidJamesMcCann@gmail.com>
2019-05-13 15:23:36 +0930

    finalised tutorial

--- Assets/TuteController.cs
+++ Assets/TuteController.cs
@@ -1,41 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using VRStandardAssets.Utils;
 using Pixelplacement;
 using TMPro;
 
 //class to control the tutorial
+//this was made in the last day of dev for this project. Expect a mess - David McCann
 public class TuteController : MonoBehaviour {
 
     [Header("Tutorial Instructions and related objects")]
     public List<Instruction> tuteInstructions = new List<Instruction>();
 
+    [Header("reference to the Interactive Item Controller")]
+    [SerializeField] protected VRInput c_Input;
+
     protected TextMeshPro textMesh;
     protected int listIndex = 0;
+    protected bool canvasDisplayed = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         textMesh = GetComponent<TextMeshPro>();
         textMesh.text = tuteInstructions[listIndex].text; //the first set of instructions in the tutorial
-        
-       
-	}
+
+
+        c_Input.OnHold += FirstStep;
+    }
+  
+    //this is hacky but also tme constraints on Tutorial creation
+    public void FirstStep()
+    {
+        if(!canvasDisplayed)
+        {
+            canvasDisplayed = true;
+            ProgressInstructions();
+        }
+    }
 
     //when the user completes the correct action, this function is called
     public void ProgressInstructions()
     {
-        //deactivate the old stuff
-        if(tuteInstructions[listIndex].linkedObject != null)
-        {
-            tuteInstructions[listIndex].linkedObject.SetActive(false);
-        }
+
         listIndex++;
         //update objects
         textMesh.text = tuteInstructions[listIndex].text;
-        if(tuteInstructions[listIndex].linkedObject != null)
-        {
-            tuteInstructions[listIndex].linkedObject.SetActive(true);
-        }
+
     }
 
 }
@@ -46,5 +58,7 @@ public class Instruction
     [Tooltip("the text that will display for this instruction")]
     public string text;
     [Tooltip("the object this instruction turns on")]
-    public DisplayObject linkedObject;
+    public GameObject linkedObject;
+    [HideInInspector]
+    public DisplayObject display;
 }
